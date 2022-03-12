44ec7447de43658224282249ef9e8bb7ad3717f5
Rishav Kumar <kumarrishav14@gmail.com>
2022-03-12 17:32:35 +0530

    Adds new plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,6 +43,8 @@ module.exports = {
 		"gatsby-transformer-remark",
 		"gatsby-transformer-sharp",
 		"gatsby-plugin-react-helmet",
+		`gatsby-plugin-preact`,
+		"gatsby-plugin-webpack-bundle-analyser-v2",
 		"gatsby-plugin-sharp",
 		{
 			resolve: "gatsby-source-contentful",
